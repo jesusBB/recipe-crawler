330dcad10f66ea6c7eccccad5b9898893e863d38
Jesus Bustillo <jesus.bustillo.barrios@gmail.com>
2022-09-28 16:28:15 +0100

    Changing way to use AWS credentials

--- src/main/java/com/foodplanner/recipe/crawler/recipecrawler/repositories/S3Repository.java
+++ src/main/java/com/foodplanner/recipe/crawler/recipecrawler/repositories/S3Repository.java
@@ -1,50 +1,34 @@
 package com.foodplanner.recipe.crawler.recipecrawler.repositories;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class S3Repository {
 
+    @Value(value = "${cloud.aws.credentials.profile-name}")
+    String awsProfile;
     @Autowired
     S3RepositoryConfiguration s3RepoConfig;
-
     public AmazonS3 getS3Client() {
-        AWSCredentials awsCredentials = new BasicAWSCredentials(
+
+        ProfileCredentialsProvider awsCredentialsProvider = new ProfileCredentialsProvider(awsProfile);
+
+        /*AWSCredentials awsCredentials = new BasicAWSCredentials(
                 s3RepoConfig.getAccessKey(),
                 s3RepoConfig.getSecretKey());
-        return AmazonS3ClientBuilder
+        /*return AmazonS3ClientBuilder
                 .standard()
                 .withRegion(s3RepoConfig.getRegion())
                 .withCredentials(
                         new AWSStaticCredentialsProvider(awsCredentials))
-                .build();
+                .build();*/
+        return AmazonS3ClientBuilder.standard()./*withRegion(s3RepoConfig.getRegion()).*/withCredentials(awsCredentialsProvider).build();
+
     }
 
-    /*public void uploadFile(HashSet<String> hashSet, String bucketName,
-            String fileName) {
-        Path path = Paths.get(bucketName + fileName + ".txt");
-
-        try {
-            Files.write(path, hashSet);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        AmazonS3 s3Client = this.getS3Client();
-        if (!s3Client.doesBucketExistV2(bucketName)) {
-            s3Client.createBucket(bucketName);
-        }
-
-        PutObjectRequest putObjectRequest = new PutObjectRequest(
-                bucketName, fileName, path.toFile())
-                        .withCannedAcl(CannedAccessControlList.PublicRead);
-
-        s3Client.putObject(putObjectRequest);
-    }*/
 }
--- src/main/java/com/foodplanner/recipe/crawler/recipecrawler/validators/JamieOliverLinksValidator.java
+++ src/main/java/com/foodplanner/recipe/crawler/recipecrawler/validators/JamieOliverLinksValidator.java
@@ -15,8 +15,11 @@ public class JamieOliverLinksValidator extends GenericValidator<String> {
     private static final Pattern END_WITH_REC_PAGE = Pattern.compile(
             "^(https://www\\.jamieoliver\\.com/recipes|/recipes).*(?<!(avi|png|(jpg))|css|pdf|(js)|(rec-page=[0-9])).*(?<!\\d)$");
 
+    private static final Pattern STARTS_WITH_HTTPS = Pattern.compile("^(https).*");
+
     static {
         VALIDATORS.add(url -> END_WITH_REC_PAGE.matcher(url).matches());
+        VALIDATORS.add(url -> STARTS_WITH_HTTPS.matcher(url).matches());
     }
 
     public JamieOliverLinksValidator() {
